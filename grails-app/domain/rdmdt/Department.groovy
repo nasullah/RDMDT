f3a6a6bad0bcf83dfa45fa2c54d23348ce518795
Nasullah Alham <nasullah.khalidalham@ndcls.ox.ac.uk>
2016-07-12 14:26:29 +0100

    Department is added

---
+++ grails-app/domain/rdmdt/Department.groovy
@@ -0,0 +1,22 @@
+package rdmdt
+
+/**
+ * Department
+ * A domain class describes the data object and it's mapping to the database
+ */
+class Department {
+
+    static constraints = {
+        departmentName()
+    }
+
+    String departmentName
+
+    /*
+     * Methods of the Domain Class
+     */
+	@Override	// Override toString for a nicer / more descriptive UI
+	public String toString() {
+		return "${departmentName}";
+	}
+}
