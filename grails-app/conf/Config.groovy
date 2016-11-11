d19b9b404af5bd3d167f8964d9cb62d6c8e1311f
Nasullah Alham <nasullah.khalidalham@ndcls.ox.ac.uk>
2016-11-11 16:43:50 +0000

    Changed GSP settings to fix date display on filterpane

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -46,22 +46,24 @@ grails.views.default.codec = "html"
 grails.controllers.defaultScope = 'singleton'
 
 // GSP settings
-grails {
-    views {
-        gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
-            codecs {
-                expression = 'html' // escapes values inside ${}
-                scriptlet = 'html' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
-            }
-        }
-        // escapes all not-encoded output at final stage of outputting
-        // filteringCodecForContentType.'text/html' = 'html'
-    }
-}
+//grails {
+//    views {
+//        gsp {
+//            encoding = 'UTF-8'
+//            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+//            codecs {
+//                expression = 'html' // escapes values inside ${}
+//                scriptlet = 'html' // escapes output from scriptlets in GSPs
+//                taglib = 'none' // escapes output from taglibs
+//                staticparts = 'none' // escapes output from static template parts
+//            }
+//        }
+//        // escapes all not-encoded output at final stage of outputting
+//        // filteringCodecForContentType.'text/html' = 'html'
+//    }
+//}
+
+grails.views.default.codec = "none" // none, html, base64 grails.views.gsp.encoding = "UTF-8" grails.converters.encoding = "UTF-8"
 
 
 grails.converters.encoding = "UTF-8"
